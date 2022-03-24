be8bf03e9279ac54ebf773052e2d0ab0bc1ec7d5
paddy117 <patrickbroere9@gmail.com>
2022-03-24 15:40:17 +0100

    ???

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import logo from './stimular.png';
 
 
 function Header() {
   return (
     <div className="App">
       <header className="App-header">
-        <h>STIMULAR</h>
+        <img src = {logo}></img>
         
       </header>
 
