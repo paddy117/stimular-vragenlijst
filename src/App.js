b70c2a0d9dc1796dccaff073ea87a929a6251278
paddy117 <patrickbroere9@gmail.com>
2022-03-25 11:37:53 +0100

    Merge branch 'main' of https://github.com/JakkoAndeweg/stimular-vragenlijst into Patrick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Collapse} from 'react-collapse';
+import collapse from './dropdown.png';
 import './App.css';
 
 // function App() {
@@ -26,12 +27,18 @@ import './App.css';
 //      <Welcome name="Sara" />
 
      
-      
+
 
 class App extends React.Component {
+
+  static defaultProps = {
+    isOpened: true
+  };
+
   constructor(props) {
     super(props);
     this.state = {value: '',
+    
     check: true};
 
     this.handleChange = this.handleChange.bind(this);
@@ -54,6 +61,7 @@ class App extends React.Component {
     
   }
 
+  
 
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.user +
@@ -62,17 +70,29 @@ class App extends React.Component {
     event.preventDefault();
   }
 
-  
+    
 
   render() {
+    const {isOpened} = this.state;
     return (
       
       
-    
+      
+      
         
       
-
+      
       <form onSubmit={this.handleSubmit}>
+        <div className="vraag">
+        {/* <img src = {collapse} onClick={onChange={}}></img> */}
+           <input
+              className="checkboxinvis"
+              type="checkbox"
+              id='checkbox1'
+              checked={isOpened}
+              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+              <label for="checkbox1"><img src={collapse} className="imgButton" /></label>
+        <Collapse isOpened={isOpened}>
         <div className="vraag">
         <h>Vraag 1</h>
         <p>dit is vraag 1</p>
@@ -98,6 +118,8 @@ class App extends React.Component {
       </div>
 
         <br />
+        </div>
+        </Collapse>
         </div>
         <br />
         <div className="vraag">
